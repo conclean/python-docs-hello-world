e481e4874ef136429c53b2d4dced6989e76b6149
conclean <lijikun_mail@foxmail.com>
2021-04-29 16:20:57 +0800

    1
    tryblogtest

--- app.py
+++ app.py
@@ -1,6 +1,53 @@
 from flask import Flask
+from flask import render_template
+
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello, 李霁琨!"
+
+@app.route('/')
+@app.route('/index')
+def index():
+    user = {'username': 'Vivek'}
+    return render_template('index.html', title='Home', user=user)
+
+
+@app.route('/if')
+def conditional():
+    user = {'username': 'Vivek'}
+    return render_template('conditional.html', user=user)
+
+
+@app.route('/loop')
+def posts():
+    user = {'username': 'Vivek'}
+    posts = [
+        {
+            'author': {'username': 'Abey'},
+            'body': 'The Avengers movie was so cool!'
+        },
+        {
+            'author': {'username': 'Seán'},
+            'body': 'The All-Blacks win again!'
+        }
+    ]
+    return render_template('loops.html', title='Home', user=user, posts=posts)
+
+
+@app.route('/template')
+def inherit():
+    user = {'username': 'Vivek'}
+    posts = [
+        {
+            'author': {'username': 'Abey'},
+            'body': 'The Avengers movie was so cool!'
+        },
+        {
+            'author': {'username': 'Seán'},
+            'body': 'The All-Blacks win again!'
+        }
+    ]
+    return render_template('extended.html', title='Home', user=user, posts=posts)
+
+
+if __name__ == '__main__':
+    app.run()
